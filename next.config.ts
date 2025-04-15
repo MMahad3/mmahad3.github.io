5f77913d3f00d5996ec741d794843f074e083f5d
MMahad3 <mahadmunir6@gmail.com>
2025-04-15 22:01:28 +0500

    update

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.NODE_ENV === 'production';
+
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: isGithubPages ? '/mahadmunir' : '',
-  assetPrefix: isGithubPages ? '/mahadmunir/' : '',
-  images: {
-    unoptimized: true,
-  },
+ 
+
 };
 //sad
 
